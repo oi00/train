a1a2de4a4a4e7b7eba8fec79b1a6d86d28f61796
門井東光 <oikazesantouhei@oikazesantouheinoMac.local>
2019-09-23 17:23:31 +0900

    maven学習

--- eclipse-maven-app/src/main/java/com/first/eclipse_maven_app/App.java
+++ eclipse-maven-app/src/main/java/com/first/eclipse_maven_app/App.java
@@ -4,10 +4,20 @@ package com.first.eclipse_maven_app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App().printMessage("Taro");
+    }
+
+    public String getMessage(String name) {
+    	return "Hi," +  name + ", Welcome to Maven Wordld!";
+    }
+
+    public void printMessage(String name) {
+    	System.out.println("/n****** App class ******/n");
+    	System.out.println(this.getMessage(name));
+    	System.out.println("/n****** App class ******/n");
     }
 }
